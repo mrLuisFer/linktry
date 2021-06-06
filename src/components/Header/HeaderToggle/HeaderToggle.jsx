718e31f7432f1b17340d9d86285647a0ae095189
mrLuisFer <lolesuncrak@gmail.com>
2021-06-06 17:06:26 -0500

    ✨: userNoSelect class added
    🐞: blue highlight in toggle fixed

--- src/components/Header/HeaderToggle/HeaderToggle.jsx
+++ src/components/Header/HeaderToggle/HeaderToggle.jsx
@@ -18,33 +18,31 @@ export default function HeaderToggle() {
   }
 
   return (
-    <div>
-      <div className='flex flex-col'>
-        <label
-          htmlFor='toggleInput'
-          className='inline-flex items-center cursor-pointer'
-        >
-          <span className='relative'>
-            <span className='block w-10 h-6 bg-gray-400 rounded-full shadow-inner'></span>
-            <span
-              className={`absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out transform
+    <div className='flex flex-col userNoSelect'>
+      <label
+        htmlFor='toggleInput'
+        className='inline-flex items-center cursor-pointer'
+      >
+        <span className='relative'>
+          <span className='block w-10 h-6 bg-gray-400 rounded-full shadow-inner'></span>
+          <span
+            className={`absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out transform
                 ${isDarkMode ? 'translate-x-4' : '-translate-x-0'}
             `}
-            >
-              <input
-                className='absolute opacity-0 w-0 h-0'
-                id='toggleInput'
-                name='toggleInput'
-                type='checkbox'
-                onChange={handleClickToggleDarkMode}
-              />
-            </span>
+          >
+            <input
+              className=' absolute opacity-0 w-0 h-0'
+              id='toggleInput'
+              name='toggleInput'
+              type='checkbox'
+              onChange={handleClickToggleDarkMode}
+            />
           </span>
-          <span className='ml-3 text-xl'>
-            {isDarkMode ? <FaLightbulb /> : <RiMoonClearFill />}
-          </span>
-        </label>
-      </div>
+        </span>
+        <span className='ml-3 text-xl'>
+          {isDarkMode ? <FaLightbulb /> : <RiMoonClearFill />}
+        </span>
+      </label>
     </div>
   )
 }
