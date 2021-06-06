591978b4d958d30eca0787b5a121039c36424e12
mrLuisFer <lolesuncrak@gmail.com>
2021-06-06 18:03:23 -0500

    🐞: html/jsx separated for a better reading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import HeaderToggle from './HeaderToggle/HeaderToggle'
 export default function Header() {
   return (
     <div className='flex items-center justify-between pt-5 px-4'>
-      <h1>React - Linktre</h1>
+      <h1 className='text-base font-medium dark:text-white cursor-pointer opacity-80 hover:opacity-100 transition'>
+        ⚛ Linktre
+      </h1>
       <HeaderToggle />
     </div>
   )
--- src/components/Header/HeaderToggle/HeaderToggle.jsx
+++ src/components/Header/HeaderToggle/HeaderToggle.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { RiMoonClearFill } from 'react-icons/ri'
 import { FaLightbulb } from 'react-icons/fa'
 
+import HeaderToggleHtml from './HeaderToggleHtml'
+
 export default function HeaderToggle() {
   const [isDarkMode, setIsDarkMode] = useState(false)
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
 
+  const body = document.body
   const lightIcon = <RiMoonClearFill />
   const darkIcon = <FaLightbulb />
   const [icon, setIcon] = useState(lightIcon)
-  const body = document.body
 
   const handleClickToggleDarkMode = (event) => {
     const isChecked = event.target.checked
@@ -19,10 +21,12 @@ export default function HeaderToggle() {
       body.classList.add('dark')
       localStorage.setItem('theme', 'dark')
       setTheme('dark')
+      setIcon(darkIcon)
     } else {
       body.classList.remove('dark')
       localStorage.setItem('theme', 'light')
       setTheme('light')
+      setIcon(lightIcon)
     }
   }
 
@@ -46,29 +50,12 @@ export default function HeaderToggle() {
   }, [])
 
   return (
-    <div className='flex flex-col userNoSelect'>
-      <label
-        htmlFor='toggleInput'
-        className='inline-flex items-center cursor-pointer'
-      >
-        <span className='relative'>
-          <span className='block w-10 h-6 bg-gray-400 rounded-full shadow-inner'></span>
-          <span
-            className={`absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out transform
-                ${isDarkMode ? 'translate-x-4' : '-translate-x-0'}
-            `}
-          >
-            <input
-              className=' absolute opacity-0 w-0 h-0'
-              id='toggleInput'
-              name='toggleInput'
-              type='checkbox'
-              onChange={handleClickToggleDarkMode}
-            />
-          </span>
-        </span>
-        <span className='ml-3 text-xl'>{icon}</span>
-      </label>
-    </div>
+    <>
+      <HeaderToggleHtml
+        isDarkMode={isDarkMode}
+        handleClickToggleDarkMode={handleClickToggleDarkMode}
+        icon={icon}
+      />
+    </>
   )
 }
