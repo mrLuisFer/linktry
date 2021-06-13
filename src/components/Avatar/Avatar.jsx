4bb84e96a2487af21b2ed857d1b93243886c25af
mrLuisFer <mrluisfeer@gmail.com>
2021-06-12 20:36:07 -0500

    fix: adding a loader when load de avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { FaRegUserCircle } from 'react-icons/fa'
-
+import Loader from '../Loader/Loader'
 import { userConfig } from '../../_Config/userConfig'
 
 export default function Avatar() {
   const [isErrorAvatarUrl, setIsErrorAvatarUrl] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const avatarUrl = userConfig.avatar
 
   const urlRegex = new RegExp(
@@ -24,6 +24,7 @@ export default function Avatar() {
           console.log(response.statusText)
           setIsErrorAvatarUrl(true)
         }
+        setIsLoading(false)
         return response
       })
       .catch((error) => {
@@ -37,21 +38,35 @@ export default function Avatar() {
     // eslint-disable-next-line
   }, [])
 
+  // Cleaning the loader
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className='py-8 flex flex-col'>
-      {avatarUrl.match(urlRegex) ? (
-        <img
-          src={isErrorAvatarUrl ? defaultImg : avatarUrl}
-          alt={altUsernameImg}
-          title={
-            isErrorAvatarUrl
-              ? 'An error loading your avatar image'
-              : altUsernameImg
-          }
-          className='block mx-auto w-36 h-36 rounded-full transition transform hover:scale-105'
-        />
+      {isLoading ? (
+        <Loader />
       ) : (
-        <FaRegUserCircle className='block mx-auto text-7xl bg-blue-700 p-1 radius text-gray-100 rounded-full' />
+        <>
+          {avatarUrl.match(urlRegex) ? (
+            <img
+              src={isErrorAvatarUrl ? defaultImg : avatarUrl}
+              alt={altUsernameImg}
+              title={
+                isErrorAvatarUrl
+                  ? 'An error loading your avatar image'
+                  : altUsernameImg
+              }
+              className='block mx-auto w-36 h-36 rounded-full transition transform hover:scale-105'
+            />
+          ) : (
+            <Loader />
+          )}
+        </>
       )}
       <h1
         className='text-center font-semibold mx-auto mt-6 text-2xl border-b-4 border-green-500 inline-block w-auto'
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../../styles/loader.css'
+
+export default function Loader() {
+  return <div className='loader mx-auto'></div>
+}
