b0fb5a253f77671eba31a92282cc7d292c27e623
mrLuisFer <mrluisfeer@gmail.com>
2021-06-21 23:08:29 -0500

    Sepafix: rating the logic when setting the darkmode

--- src/components/Header/HeaderToggle/HeaderToggle.jsx
+++ src/components/Header/HeaderToggle/HeaderToggle.jsx
@@ -1,55 +1,12 @@
-import React, { useEffect, useState } from "react"
-import { RiMoonClearFill } from "react-icons/ri"
-import { FaLightbulb } from "react-icons/fa"
+import React, { useState } from "react"
 import HeaderToggleHtml from "./HeaderToggleHtml"
 import RecomendationMsg from "../RecomendationMsg/RecomendationMsg"
+import { useSetDarkMode } from "./hook/useSetDarkMode"
 
 export default function HeaderToggle() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
-
-  const body = document.body
-  const lightIcon = <RiMoonClearFill title='Active dark mode' />
-  const darkIcon = <FaLightbulb title='Active light mode' />
-  const [icon, setIcon] = useState(lightIcon)
   const [closeMsg, setCloseMsg] = useState(false)
 
-  const handleClickToggleDarkMode = (event) => {
-    const isChecked = event.target.checked
-    setIsDarkMode(isChecked)
-
-    if (isChecked) {
-      body.classList.add("dark")
-      localStorage.setItem("theme", "dark")
-      setTheme("dark")
-      setIcon(darkIcon)
-    } else {
-      body.classList.remove("dark")
-      localStorage.setItem("theme", "light")
-      setTheme("light")
-      setIcon(lightIcon)
-    }
-  }
-
-  const checkTheme = () => {
-    if (!theme) {
-      localStorage.setItem("theme", "light")
-    }
-    if (theme === "dark") {
-      body.classList.add("dark")
-      setIcon(darkIcon)
-      setIsDarkMode(true)
-    } else {
-      body.classList.remove("dark")
-      setIcon(lightIcon)
-      setIsDarkMode(false)
-    }
-  }
-
-  useEffect(() => {
-    checkTheme()
-    // eslint-disable-next-line
-  }, [])
+  const { isDarkMode, handleClickToggleDarkMode, icon } = useSetDarkMode()
 
   return (
     <div className='relative'>
---
+++ src/components/Header/HeaderToggle/hook/useSetDarkMode.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { RiMoonClearFill } from "react-icons/ri"
+import { FaLightbulb } from "react-icons/fa"
+
+export const useSetDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
+
+  const body = document.body
+  const lightIcon = <RiMoonClearFill title='Active dark mode' />
+  const darkIcon = <FaLightbulb title='Active light mode' />
+  const [icon, setIcon] = useState(lightIcon)
+
+  const handleClickToggleDarkMode = (event) => {
+    const isChecked = event.target.checked
+    setIsDarkMode(isChecked)
+
+    if (isChecked) {
+      body.classList.add("dark")
+      localStorage.setItem("theme", "dark")
+      setTheme("dark")
+      setIcon(darkIcon)
+    } else {
+      body.classList.remove("dark")
+      localStorage.setItem("theme", "light")
+      setTheme("light")
+      setIcon(lightIcon)
+    }
+  }
+
+  const checkTheme = () => {
+    if (!theme) {
+      localStorage.setItem("theme", "light")
+    }
+    if (theme === "dark") {
+      body.classList.add("dark")
+      setIcon(darkIcon)
+      setIsDarkMode(true)
+    } else {
+      body.classList.remove("dark")
+      setIcon(lightIcon)
+      setIsDarkMode(false)
+    }
+  }
+
+  useEffect(() => {
+    checkTheme()
+    // eslint-disable-next-line
+  }, [])
+
+  return {
+    isDarkMode,
+    handleClickToggleDarkMode,
+    icon,
+  }
+}
