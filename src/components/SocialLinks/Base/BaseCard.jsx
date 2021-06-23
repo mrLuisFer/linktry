3d80754bdac7f3007440303aaef032ddc2b0f0fe
mrLuisFer <mrluisfeer@gmail.com>
2021-06-23 08:45:07 -0500

    fix: Adding more accesibility for the links and urls

--- src/components/SocialLinks/Base/BaseCard.jsx
+++ src/components/SocialLinks/Base/BaseCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { AiFillHeart } from 'react-icons/ai'
-import { HiOutlineExternalLink } from 'react-icons/hi'
-import AnchorTag from '../../AnchorTag/AnchorTag'
+import React from "react"
+import { AiFillHeart } from "react-icons/ai"
+import { HiOutlineExternalLink } from "react-icons/hi"
+import AnchorTag from "../../AnchorTag/AnchorTag"
 
 /**
  *
@@ -16,57 +16,62 @@ import AnchorTag from '../../AnchorTag/AnchorTag'
  * @returns JSX.Element
  */
 export default function BaseCard({
-  username = '',
-  url = '',
-  description = '',
+  username = "",
+  url = "",
+  description = "",
   jsxIcon = <AiFillHeart />,
-  customUsernameClasses = '',
-  customIconClasses = '',
-  customCardClasses = '',
-  customUrlIconClasses = '',
+  customUsernameClasses = "",
+  customIconClasses = "",
+  customCardClasses = "",
+  customUrlIconClasses = "",
 }) {
   return (
     <>
       {username.length > 0 ? (
         <div
-          className={`bg-gray-200 dark:bg-gray-700 p-4 rounded-lg my-6 sm:my-8 md:my-12 transition-shadow hover:shadow-md removeBlueHighlight hoverBaseCard ${customCardClasses} w-64 mb:w-10/12 sm:w-full mx-auto`}
+          className={`
+              bg-gray-200 dark:bg-gray-700 p-4 rounded-lg my-6 sm:my-8 md:my-12 transition-shadow hover:shadow-md removeBlueHighlight hoverBaseCard ${customCardClasses} w-64 mb:w-10/12 sm:w-full mx-auto
+              focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 group
+            `}
         >
-          <div className='flex gap-x-4'>
-            <div
-              draggable='false'
-              className={`text-4xl md:text-5xl h-full my-auto mx-0 transform hover:scale-110 ${customIconClasses}`}
-            >
-              <AnchorTag hrefUrl={url} customClasses='text-white'>
-                {jsxIcon}
-              </AnchorTag>
-            </div>
-            <div className='w-full'>
+          <AnchorTag hrefUrl={url}>
+            <div className='flex gap-x-4'>
               <div
-                className={`flex justify-between items-center ${
-                  description.length > 5 ? '' : 'h-full'
-                }`}
+                draggable='false'
+                className={`text-4xl md:text-5xl h-full my-auto mx-0 transform hover:scale-110 ${customIconClasses}`}
               >
-                <h1
-                  className={`font-semibold text-lg ${customUsernameClasses}`}
-                >
-                  {username.length > 0 ? username : 'Username'}
-                </h1>
-                <AnchorTag>
-                  <HiOutlineExternalLink
-                    className={`text-xl sm:text-2xl transform hover:scale-110 cursor-pointer ${customUrlIconClasses}`}
-                  />
+                <AnchorTag hrefUrl={url} customClasses='text-white'>
+                  {jsxIcon}
                 </AnchorTag>
               </div>
-              {description.length > 5 ? (
-                <p className='text-sm'>{description}</p>
-              ) : (
-                ''
-              )}
+              <div className='w-full'>
+                <div
+                  className={`flex justify-between items-center ${
+                    description.length > 5 ? "" : "h-full"
+                  }`}
+                >
+                  <h1
+                    className={`font-semibold text-lg text-black ${customUsernameClasses} group-hover:underline`}
+                  >
+                    {username.length > 0 ? username : "Username"}
+                  </h1>
+                  <AnchorTag hrefUrl={url}>
+                    <HiOutlineExternalLink
+                      className={`text-xl sm:text-2xl transform hover:scale-110 cursor-pointer ${customUrlIconClasses}`}
+                    />
+                  </AnchorTag>
+                </div>
+                {description.length > 5 ? (
+                  <p className='text-sm'>{description}</p>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
-          </div>
+          </AnchorTag>
         </div>
       ) : (
-        ''
+        ""
       )}
     </>
   )
