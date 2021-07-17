14beff072927c5a28766639fd529d4e94c3dda1a
mrLuisFer <mrluisfeer@gmail.com>
2021-07-17 17:27:51 -0500

    feat: creating a maxLength for check de username

--- src/hooks/useGetFullName.js
+++ src/hooks/useGetFullName.js
@@ -2,12 +2,13 @@ export const useGetFullName = (userConfig = {}) => {
   const firstNameLength = userConfig.firstName.length
   const lastNameLength = userConfig.lastName.length
   let fullName = ''
+  const maxLength = 10
 
-  if (lastNameLength > 10) {
+  if (lastNameLength > maxLength) {
     fullName = `${userConfig.firstName}`
-  } else if (firstNameLength > 10) {
+  } else if (firstNameLength > maxLength) {
     fullName = `${userConfig.username}`
-  } else if (firstNameLength > 10 && lastNameLength > 10) {
+  } else if (firstNameLength > maxLength && lastNameLength > maxLength) {
     fullName = `${userConfig.username}`
   }
   fullName = `${userConfig.firstName} ${userConfig.lastName}`
