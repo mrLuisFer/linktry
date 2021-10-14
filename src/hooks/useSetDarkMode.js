5c943292fc460972683de19c90f6bdff35a32ab9
mrLuisFer <mrluisfeer@gmail.com>
2021-10-14 09:37:59 -0500

    [fix] change if/else estatement to ternary operator

--- src/hooks/useSetDarkMode.js
+++ src/hooks/useSetDarkMode.js
@@ -2,45 +2,41 @@ import { useEffect, useState } from 'react'
 import { RiMoonClearFill } from 'react-icons/ri'
 import { FaLightbulb } from 'react-icons/fa'
 
+const body = document.body
+const lightIcon = <RiMoonClearFill title='Active dark mode' />
+const darkIcon = <FaLightbulb title='Active light mode' />
+
 export const useSetDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(false)
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
-
-  const body = document.body
-  const lightIcon = <RiMoonClearFill title='Active dark mode' />
-  const darkIcon = <FaLightbulb title='Active light mode' />
   const [icon, setIcon] = useState(lightIcon)
 
   const handleClickToggleDarkMode = (event) => {
     const isChecked = event.target.checked
     setIsDarkMode(isChecked)
-
     if (isChecked) {
       body.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
-      setTheme('dark')
-      setIcon(darkIcon)
     } else {
       body.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
-      setTheme('light')
-      setIcon(lightIcon)
     }
+    localStorage.setItem('theme', isChecked ? 'dark' : 'light')
+    setTheme(isChecked ? 'dark' : 'light')
+    setIcon(isChecked ? darkIcon : lightIcon)
   }
 
   const checkTheme = () => {
     if (!theme) {
       localStorage.setItem('theme', 'light')
     }
-    if (theme === 'dark') {
+
+    const isDarkTheme = theme === 'dark'
+    if (isDarkTheme) {
       body.classList.add('dark')
-      setIcon(darkIcon)
-      setIsDarkMode(true)
     } else {
       body.classList.remove('dark')
-      setIcon(lightIcon)
-      setIsDarkMode(false)
     }
+    setIcon(isDarkTheme ? darkIcon : lightIcon)
+    setIsDarkMode(isDarkTheme)
   }
 
   useEffect(() => {
