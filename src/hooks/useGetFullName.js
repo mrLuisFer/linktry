946eadd240db7db3f2ad6108c3a0e362a6b0dcd2
mrLuisFer <mrluisfeer@gmail.com>
2021-10-14 09:24:43 -0500

    [fix] reduce unnecessary validations in useGetFullName

--- src/hooks/useGetFullName.js
+++ src/hooks/useGetFullName.js
@@ -1,19 +1,9 @@
 export const useGetFullName = (
   userConfig = { firstName: '', username: '', lastName: '' }
 ) => {
-  const firstNameLength = userConfig.firstName.length
-  const lastNameLength = userConfig.lastName.length
-  let fullName = ''
-  const maxLength = 10
-
-  if (lastNameLength > maxLength) {
-    fullName = `${userConfig.firstName}`
-  } else if (firstNameLength > maxLength) {
-    fullName = `${userConfig.username}`
-  } else if (firstNameLength > maxLength && lastNameLength > maxLength) {
-    fullName = `${userConfig.username}`
+  const fullName = `${userConfig.firstName} ${userConfig.lastName}`
+  if (fullName.length > 30) {
+    return userConfig.username
   }
-  fullName = `${userConfig.firstName} ${userConfig.lastName}`
-
   return fullName
 }
