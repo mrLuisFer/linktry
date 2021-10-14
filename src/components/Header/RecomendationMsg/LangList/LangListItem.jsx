1c104b578675ba329a021755a1fba1770577648b
mrLuisFer <mrluisfeer@gmail.com>
2021-10-14 11:01:17 -0500

    [feat] add transition and color for langList

--- src/components/Header/RecomendationMsg/LangList/LangListItem.jsx
+++ src/components/Header/RecomendationMsg/LangList/LangListItem.jsx
@@ -2,7 +2,7 @@ export default function LangListItem({ lang = '', setLanguage }) {
   return (
     <li
       onClick={() => setLanguage(`${lang.toLocaleLowerCase()}`)}
-      className='capitalize text-sm transform hover:scale-110 cursor-pointer ml-2 mt-2 select-none'
+      className='capitalize text-sm transform transition hover:text-green-300 hover:scale-110 cursor-pointer ml-2 mt-2 select-none'
     >
       {lang}
     </li>
