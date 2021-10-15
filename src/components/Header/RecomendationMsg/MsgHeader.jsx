d3a1f9e41ac9cafc6bbe800b0ecbe386ed10f02e
mrLuisFer <mrluisfeer@gmail.com>
2021-10-15 00:09:03 -0500

    [fix] create MsgHeader for a easy read
    [chore] add framer motion

---
+++ src/components/Header/RecomendationMsg/MsgHeader.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { IoLanguageSharp, IoCloseSharp } from 'react-icons/io5'
+import LangList from './LangList/LangList'
+
+const MsgHeader = ({ setLanguage, handleCloseMsg }) => {
+  const [showLangs, setShowLangs] = useState(false)
+
+  return (
+    <section className='flex justify-between mb-2 text-lg'>
+      <div>
+        <IoLanguageSharp
+          onClick={() => setShowLangs(!showLangs)}
+          className={`cursor-pointer transform hover:scale-110 select-none transition-colors hover:text-blue-300 ${
+            showLangs && 'text-blue-300'
+          }`}
+        />
+        {showLangs && <LangList setLanguage={setLanguage} />}
+      </div>
+      <IoCloseSharp
+        className='text-red-400 cursor-pointer select-none transform hover:scale-110 hover:text-red-500'
+        onClick={handleCloseMsg}
+      />
+    </section>
+  )
+}
+
+export default MsgHeader
--- src/components/Header/RecomendationMsg/RecomendationMsg.jsx
+++ src/components/Header/RecomendationMsg/RecomendationMsg.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import { IoLanguageSharp, IoCloseSharp } from 'react-icons/io5'
-import LangList from './LangList/LangList'
 import { langs } from './langs'
+import MsgHeader from './MsgHeader'
 
 export default function RecomendationMsg({ setCloseMsg }) {
   const [language, setLanguage] = useState('english')
-  const [showLangs, setShowLangs] = useState(false)
 
   const handleCloseMsg = () => {
     setCloseMsg(true)
@@ -14,21 +12,7 @@ export default function RecomendationMsg({ setCloseMsg }) {
 
   return (
     <div className='absolute right-0 w-52 text-sm bg-gray-700 text-white p-3 rounded-lg mt-3 z-10 shadow-md hover:shadow-lg'>
-      <div className='flex justify-between mb-2 text-lg'>
-        <div>
-          <IoLanguageSharp
-            onClick={() => setShowLangs(!showLangs)}
-            className={`cursor-pointer transform hover:scale-110 select-none transition-colors hover:text-blue-300 ${
-              showLangs && 'text-blue-300'
-            }`}
-          />
-          {showLangs && <LangList setLanguage={setLanguage} />}
-        </div>
-        <IoCloseSharp
-          className='cursor-pointer select-none transform hover:scale-110 text-red-400 hover:text-red-500'
-          onClick={handleCloseMsg}
-        />
-      </div>
+      <MsgHeader setLanguage={setLanguage} handleCloseMsg={handleCloseMsg} />
       {langs[language]}
     </div>
   )
