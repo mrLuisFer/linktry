07d7c79279b6e72068fb8afd4ace951d02a3fea5
mrLuisFer <mrluisfeer@gmail.com>
2023-03-12 16:00:28 -0600

    update layout for mobile size

--- src/components/core/Cards/SkeletonCard.tsx
+++ src/components/core/Cards/SkeletonCard.tsx
@@ -6,7 +6,7 @@ export default function SkeletonCard(): JSX.Element {
     <CardMotion>
       <div className='mockup-window border bg-base-300'>
         <div className='flex justify-center bg-base-200'>
-          <Skeleton height={200} width={450} />
+          <Skeleton height={[200, 200]} width={[300, 450]} />
           <span className='absolute top-1/2 font-bold opacity-75 select-none'>
             Loading....
           </span>
--- src/components/core/Embeds/Youtube.tsx
+++ src/components/core/Embeds/Youtube.tsx
@@ -1,12 +1,19 @@
 import { YouTubeEmbed } from 'react-social-media-embed'
 import CardMotion from '../CardMotion'
+import { useMediaQuery } from '@chakra-ui/react'
 
 export default function YoutubeCard({ url }: { url: string }): JSX.Element {
+  const [isLargerThanMobile] = useMediaQuery('minWidth: 400px')
+
   return (
     <CardMotion>
       <div className='mockup-window border bg-base-300 h-fit'>
         <div className='flex justify-center bg-base-200'>
-          <YouTubeEmbed url={url} width={600} height={350} />
+          <YouTubeEmbed
+            url={url}
+            width={!isLargerThanMobile ? 310 : 600}
+            height={!isLargerThanMobile ? 300 : 350}
+          />
         </div>
       </div>
     </CardMotion>
